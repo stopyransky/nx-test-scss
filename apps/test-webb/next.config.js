502f805fb0883c8f0527f42136a65ab9ac47e20a
Karol Stopyra <stopyransky@gmail.com>
2021-10-01 22:31:13 +0200

    clean next.config;

--- apps/test-webb/next.config.js
+++ apps/test-webb/next.config.js
@@ -1,26 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const withNx = require('@nrwl/next/plugins/with-nx');
-
-// /**
-//  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
-//  **/
-// const nextConfig = {
-//   nx: {
-//     // Set this to true if you would like to to use SVGR
-//     // See: https://github.com/gregberge/svgr
-//     svgr: false,
-//   },
-// };
-
-// module.exports = withNx(nextConfig);
-
-// next.config.js
-// https://github.com/vercel/next.js/blob/canary/packages/next/server/config-shared.ts#L68
-
 const withNx = require('@nrwl/next/plugins/with-nx');
 const withPlugins = require('next-compose-plugins');
 
-// https://www.npmjs.com/package/next-transpile-modules
 const withTM = require('next-transpile-modules')(['react-spring'], {
   debug: true,
 });
